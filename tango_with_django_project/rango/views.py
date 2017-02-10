f584d8d5181d334e8ec70ceea0561690d37f6160
RafaelVelazquezMartin <2197579V@student.gla.ac.uk>
2017-02-10 06:44:54 +0000

    End of Chapter 10

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -23,13 +23,15 @@ def index(request):
 
 def about(request):
 
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
+    request.session.set_test_cookie()
 
     context_dict = {}
 
-    return render(request, 'rango/about.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 
 def show_category(request, category_name_slug):
